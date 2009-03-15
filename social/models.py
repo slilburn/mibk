194102d045578c0653a8951cd2d4c7b5fdb29447
atroche <roche.a@gmail.com>
2009-03-15 14:04:04 +0800

    Added email address field for Person class.
    Signed-off-by: slilburn <simonlilburn@gmail.com>

--- social/models.py
+++ social/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=200)
+    email = models.EmailField()
     
     class Meta:
         verbose_name_plural = "people"
