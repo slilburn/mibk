8f9be8ae26cfddb9b18cf2a4355f8238ebd6c1db
Simon Lilburn <simonlilburn@gmail.com>
2009-03-15 16:52:41 +1100

    Added first class for Person/users.

--- social/models.py
+++ social/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=200)
+    
+    class Meta:
+        verbose_name_plural = "people"
+        
+    def __unicode__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+        
